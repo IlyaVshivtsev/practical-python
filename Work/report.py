e57e4cac65cd2b0e058a7b81c7a55c7ea35198ef
IlyaVshivtsev <iavshivtsev@gmail.com>
2020-10-11 14:15:45 +0300

    exercise 4.8: function main of report.py was modified with new (optional) format argument

--- Work/report.py
+++ Work/report.py
@@ -57,9 +57,12 @@ def portfolio_report(portfoliofile, pricefile, fmt='txt'):
     print_report(report, formatter)
 
 def main(args):
-    if len(args) != 3:
-        raise SystemExit('Usage: %s portfile pricefile' % args[0])
-    portfolio_report(args[1], args[2])
+    if len(args) == 3:
+        portfolio_report(args[1], args[2])
+    elif len(args) == 4:
+        portfolio_report(args[1], args[2], fmt=args[3])
+    else:
+        raise SystemExit('Usage: %s portfile pricefile [format]' % args[0])
 
 if __name__ == '__main__':
     import sys
